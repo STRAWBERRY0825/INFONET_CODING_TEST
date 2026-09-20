9f523ddf28b5e2acdd6a9b8da8b0a799b27bf379
STRAWBERRY <sj761725@naver.com>
2026-09-20 20:40:57 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 5.04 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+int solution(int n) {
+    int answer = 0;
+    for(int i=0; i<9; i++){
+        answer+=n%10;
+        n/=10;
+    }
+    return answer;
+}
